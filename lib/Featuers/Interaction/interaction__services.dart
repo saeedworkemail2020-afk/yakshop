e6315f9696a5f291339fb6f88e5e5aba2588ed7b
Saeed <saeedworkemail2020@gmail.com>
2025-09-21 18:08:30 +0400

    Clean code from renaming to deleting prints

--- lib/Featuers/Interaction/interaction__services.dart
+++ lib/Featuers/Interaction/interaction__services.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
-import 'package:amozeshyar/Featuers/Interaction/Services/service_herd.dart';
-import 'package:amozeshyar/Featuers/Interaction/Services/service_stock.dart';
+import 'package:amozeshyar/Featuers/Interaction/DataModel/datamodel_herd.dart';
+import 'package:amozeshyar/Featuers/Interaction/DataModel/datamodel_stock.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class Interactionservices {
   RxString milk = "0".obs;
   RxString skins = "0".obs;
-  RxList<ServiceHerd> herd = <ServiceHerd>[].obs;
+  RxList<DataModeleHerd> herd = <DataModeleHerd>[].obs;
   String basestockUri, baseherdUri;
   Interactionservices({required this.basestockUri, required this.baseherdUri});
   Future getstock({required days}) async {
@@ -16,8 +16,8 @@ class Interactionservices {
       var response = await http.get(uri);
 
       var result = jsonDecode(response.body);
-      milk.value = ServiceStok(amount: result).milk.toString();
-      skins.value = ServiceStok(amount: result).skins.toString();
+      milk.value = DataModelStok(amount: result).milk.toString();
+      skins.value = DataModelStok(amount: result).skins.toString();
     } catch (e) {
       print(e);
     }
@@ -30,9 +30,8 @@ class Interactionservices {
       var response = await http.get(uri);
       var result = jsonDecode(response.body);
       herd.value = [];
-
       for (var yak in result['herd']) {
-        herd.add(ServiceHerd.fromjson(yak));
+        herd.add(DataModeleHerd.fromjson(yak));
       }
       print("<====ended========");
     } catch (e) {

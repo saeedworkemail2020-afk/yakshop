f739f8896a34bc37c28d32e5664acf0187d7b433
Saeed <saeedworkemail2020@gmail.com>
2025-09-21 18:03:28 +0400

    change uri

--- lib/Core/service_config.dart
+++ lib/Core/service_config.dart
@@ -1,5 +1,5 @@
 class ServiceConfig {
-  static const String baseUri = "https://85eeffd1332d.ngrok-free.app";
+  static const String baseUri = "https://fa3567c07825.ngrok-free.app";
   static Uri urihealth = Uri.parse("$baseUri/health");
   static const String basestockUri = "$baseUri/yak-shop/stock/";
   static const String baseherdUri = "$baseUri/yak-shop/herd/";

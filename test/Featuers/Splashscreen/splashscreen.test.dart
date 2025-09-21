e7c97b3593b7f1b5a54995b70ae99c12af44839e
Saeed <saeedworkemail2020@gmail.com>
2025-09-21 18:01:59 +0400

    Unit Test splashscreen completed

--- test/Featuers/Splashscreen/splashscreen.test.dart
+++ test/Featuers/Splashscreen/splashscreen.test.dart
@@ -1,18 +1,12 @@
 import 'package:amozeshyar/Core/service_config.dart';
-import 'package:amozeshyar/Featuers/Splashscreen/splashscreen_model.dart';
 import 'package:amozeshyar/Featuers/Splashscreen/splashscreen_services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group("Splashscreen", () {
-    test("service", () {
+    test("service", () async {
       final splashscreen = SplashscreenServices(ServiceConfig.urihealth);
-      // expect(splashscreen.response.body, true);
-    });
-    test("model", () {
-      final model = SplashscreenModel();
-      model.navigate();
-      expect(model.result, true);
+      expect(await splashscreen.connectioncheck(), true);
     });
   });
 }

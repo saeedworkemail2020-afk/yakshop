1ea64c1fad5908842e1dd7ae4fe1804f0400e272
Saeed <saeedworkemail2020@gmail.com>
2025-09-24 13:26:36 +0400

    shaved converted to double

--- lib/Featuers/Interaction/DataModel/datamodel_herd.dart
+++ lib/Featuers/Interaction/DataModel/datamodel_herd.dart
@@ -1,13 +1,12 @@
 class DataModeleHerd {
   final String name;
-  final int shaved;
-  final double age;
+  final double shaved, age;
   DataModeleHerd({required this.name, required this.age, required this.shaved});
   factory DataModeleHerd.fromjson(Map<String, dynamic> json) {
     return DataModeleHerd(
       name: json['name'],
       age: json['age'],
-      shaved: json['age_last_shaved'],
+      shaved: json['age_last_shaved'].toDouble(),
     );
   }
 }
